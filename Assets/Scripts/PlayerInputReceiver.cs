7fcedf136f64a46ba99ca6b3f6ec3f71de7b2bb3
Felipe Figueiredo <al.kaef@gmail.com>
2024-05-09 00:41:37 -0300

    player movement and animations.

---
+++ Assets/Scripts/PlayerInputReceiver.cs
@@ -0,0 +1,61 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+public class PlayerInputReceiver : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    [SerializeField] private InputActionReference movementActionReference;
+    [SerializeField] private InputActionReference interactActionReference;
+
+    private InputAction _movementAction;
+    private InputAction _interactAction;
+    private Vector2 _inputMovementValue;
+
+    public UnityEvent<Vector2> onMovementInput;
+
+    private void Awake()
+    {
+        SetActions();
+    }
+
+    private void OnDisable()
+    {
+        RemoveListeners();
+    }
+
+    private void SetActions()
+    {
+        _movementAction = playerInput.actions.FindAction(movementActionReference.action.id);
+        if(_movementAction != null)
+        {
+            _movementAction.performed += MoveInput;
+            _movementAction.canceled += MoveInput;
+        }
+        
+        _interactAction = playerInput.actions.FindAction(interactActionReference.action.id);
+        if(_interactAction != null)
+            _interactAction.performed += InteractInput;
+    }
+
+    private void RemoveListeners()
+    {
+        if(_movementAction != null)
+            _movementAction.performed -= MoveInput;
+        if(_interactAction != null)
+            _interactAction.performed -= InteractInput;
+    }
+
+    private void MoveInput(InputAction.CallbackContext context)
+    {
+        _inputMovementValue = context.ReadValue<Vector2>();
+        onMovementInput?.Invoke(_inputMovementValue);
+    }
+    
+    private void InteractInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("interact");
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private PlayerInputReceiver playerInputReceiver;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private Animator animator;
+
+    private bool _isMoving;
+    private Vector2 _movementDirection;
+    private static readonly int Horizontal = Animator.StringToHash("Horizontal");
+    private static readonly int Vertical = Animator.StringToHash("Vertical");
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
+    private Rigidbody2D Rb2d => GetComponent<Rigidbody2D>();
+
+    private void Awake()
+    {
+        SetListeners();
+    }
+    
+    private void FixedUpdate()
+    {
+        ApplyMovement();
+    }
+    
+    private void SetListeners()
+    {
+        if (playerInputReceiver != null)
+        {
+            playerInputReceiver.onMovementInput.AddListener(MoveInput);
+        }
+    }
+
+    private void MoveInput(Vector2 value)
+    {
+        if (value.x != 0 && value.y != 0)
+        {
+            if (_movementDirection == Vector2.zero)
+            {
+                _movementDirection = new Vector2(value.x, 0);
+            }
+            return;
+        }
+        
+        _movementDirection = value;
+
+        if (_movementDirection == Vector2.zero)
+        {
+            _isMoving = false;
+        }
+        else
+        {
+            _isMoving = true;
+            
+            if (animator)
+            {
+                animator.SetFloat(Horizontal, _movementDirection.x);
+                animator.SetFloat(Vertical, _movementDirection.y);
+            }
+        }
+        
+        if (animator)
+        {
+            animator.SetBool(IsMoving, _isMoving);
+        }
+        
+    }
+    
+    private void ApplyMovement()
+    {
+        Rb2d.MovePosition(Rb2d.position + _movementDirection * (movementSpeed * Time.fixedDeltaTime));
+    }
+}
